fe55c578be73f392a03781c3efabf3da6e919f55
Joachim Meyer <jmeyer@boisestate.edu>
2025-08-28 14:10:36 -0600

    Setup - Bring AWSM in line with all the other modules

--- awsm/__init__.py
+++ awsm/__init__.py
@@ -1,18 +1,13 @@
-from pkg_resources import DistributionNotFound, get_distribution
-
-try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
-    __version__ = 'unknown'
-
 import os
 import smrf
+from .version import __version__
 
 __core_config__ = os.path.abspath(
-    os.path.dirname(__file__) + '/framework/CoreConfig.ini')
+    os.path.dirname(__file__) + '/framework/CoreConfig.ini'
+)
 __recipes__ = os.path.abspath(os.path.dirname(
-    __file__) + '/framework/recipes.ini')
-
+    __file__) + '/framework/recipes.ini'
+)
 __config_titles__ = {
     'awsm master': 'Configurations for AWSM Master section',
     'paths': 'Configurations for PATHS section for rigid directory work',
@@ -24,13 +19,8 @@ __config_titles__ = {
     'ipysnobal initial conditions': 'Initial condition parameters for PySnobal',
     'ipysnobal constants': 'Input constants for PySnobal'
 }
-
-
-__config_header__ = "Configuration File for AWSM {0}\n" \
-    "Using SMRF {1}\n\n" \
-    "For AWSM related help see:\n" \
-    "http://awsm.readthedocs.io/en/latest/\n" \
-    "\nFor SMRF related help see:\n" \
-    "http://smrf.readthedocs.io/en/latest/\n".format(
+__config_header__ = "Configuration File for AWSM {0}\n Using SMRF {1}\n".format(
         __version__,
-        smrf.__version__)
+        smrf.__version__,
+
+)
